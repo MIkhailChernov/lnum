efeab4875f8336de3e9572283987a4f604f77081
Mikhail <chernov89176396028@gmail.com>
2018-05-23 20:46:43 +0300

    massive suffix

--- sol.cpp
+++ sol.cpp
@@ -37,22 +37,56 @@ typedef pair<ll, ll> pll;
 typedef vector<ll> vll;
 typedef unsigned long long ull;
 typedef tree <pair<int, char>, null_type, less<pair<int, char>>, rb_tree_tag, tree_order_statistics_node_update> _tree;
+ 
+const int maxn = (int) 1e5;
+vi pn, cn;
 
-void prefix_function (string &s, vi &p) {
-	int n = s.size();
-	p.resize(n);
+void massive_suffix (vector<vi> &p, string &str) {
+	vector <vi> c;
+	int n = str.size();
+	int m = (int)log2(n) + 2;
+	p.resize(m), c.resize(m);
+	for (int i = 0; i < m; ++i) p[i].resize(n), c[i].resize(n);
+	vi cnt(256), pn(n), cn(n);
+	for (int i = 0; i < n; ++i) cnt[str[i]]++;		
+	for (int i = 1; i < cnt.size(); ++i) cnt[i] += cnt[i - 1];
+	for (int i = 0; i < n; ++i) p[0][--cnt[str[i]]] = i;
+	int classes = 1;
 	for (int i = 1; i < n; ++i) {
-		int j = p[i - 1];
-		while (j > 0 && s[i] != s[j]) j = p[j - 1];
-		if (s[i] == s[j]) ++j;
-		p[i] = j;
+		if (str[p[0][i]] != str[p[0][i - 1]]) ++classes;
+		c[0][p[0][i]] = classes - 1;
+	}
+	///
+	for (int h = 0; (1 << h) < n; ++h) {
+		for (int i = 0; i < n; ++i) {
+			pn[i] = p[h][i] - (1 << h);
+			if (pn[i] < 0) pn[i] += n;
+		}
+		fill(all(cnt), 0);
+		for (int i = 0; i < n; ++i) ++cnt[c[h][pn[i]]];
+		for (int i = 1; i < cnt.size(); ++i) cnt[i] += cnt[i - 1];
+		for (int i = n - 1; i >= 0; --i) p[h + 1][--cnt[c[h][pn[i]]]] = pn[i];
+		classes = 1;
+		cn[p[h + 1][0]] = 0;
+		for (int i = 1; i < n; ++i) {
+			int mid1 = (p[h + 1][i] + (1 << h)) % n, mid2 = (p[h + 1][i - 1] + (1 << h)) % n;
+			if (c[h][p[h + 1][i]] != c[h][p[h + 1][i - 1]] || c[h][mid1] != c[h][mid2]) ++classes;
+			cn[p[h + 1][i]] = classes - 1;
+		}
+		copy(all(cn), c[h + 1].begin());
 	}
 }
 
 int main() {
-	string s;
-	cin >> s;
-	vi p;
-	prefix_function(s, p);
-	for (int j : p) cout << j << ' ';
+	string str;
+	cin >> str;
+	vector <vi> p;
+	massive_suffix(p, str);
+	int m = p.size();
+	fo(i, m) {
+		cout << (1 << i) << endl;
+		for (int j : p[i]) cout << j << ' ' ;
+		cout << endl;
+	}
+	re 0;
 }
